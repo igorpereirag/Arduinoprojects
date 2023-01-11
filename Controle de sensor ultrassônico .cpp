a1948aa6b52569bd33102965a58103b8226975b5
IGOR PEREIRA <97792380+Igorpereirag@users.noreply.github.com>
2023-01-11 20:12:46 -0300

    Update Controle de sensor ultrassônico .cpp

--- Controle de sensor ultrassônico .cpp
+++ Controle de sensor ultrassônico .cpp
@@ -1,9 +1,7 @@
 #define echo 6
 #define trig 7
-
 unsigned long tempo;
 double distancia;
-
 void setup() {
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
@@ -15,43 +13,38 @@ void setup() {
  digitalWrite(trig, LOW);
 unsigned long tempo;
 double distancia;
-
 }
-
 void loop() {
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
-
   tempo = pulseIn(echo, HIGH);
-
   distancia = tempo/58;
-
+  Serial.print("Distancia:");
   Serial.println(distancia);
-  
-   if(distancia <= 30.00){
-      digitalWrite(13, HIGH);
-      
-      digitalWrite(12, LOW);
-      digitalWrite(11,LOW);
-       
-
-    }
-    if(distancia > 30.00 || distancia <= 90.00 ){
-      digitalWrite(13, LOW);
-       digitalWrite(12, HIGH);
-       digitalWrite(11, LOW);
-       
-      
-       
-
-    }if(distancia > 91.00 ){
+if(distancia > 120.00){
       digitalWrite(11, HIGH);
       digitalWrite(12, LOW);
-      digitalWrite(13, LOW);
-     
+      digitalWrite(13,LOW);
+      digitalWrite(3, LOW);
     }
-    
+    if( distancia >= 60.00 and distancia <=120.00){
+      digitalWrite(13, LOW);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(3, LOW);    
+      }
+        if (distancia < 60.00 ){
+      digitalWrite(12, LOW);
+       digitalWrite(13, HIGH);
+       digitalWrite(11, LOW);      
+      int volume = map(distancia,2.00,60.00, 0,255);
+       analogWrite(3, volume);
+       Serial.print("Distancia Critica!:");
+        Serial.println(distancia);
+        
+        }
+  
  delay(300);
 
 }
